7bc0e0964761e17ba9e884563b14067605e64587
James Cuzella <james.cuzella@lyraphase.com>
2022-05-24 23:39:14 -0600

    Fix RuboCop style warning
    Rubocop Warning was:
    
        Offenses:
    
        /opt/homebrew/Library/Taps/homebrew/homebrew-cask/Casks/ndi-tools.rb:7:81: C: [Correctable] Cask/Desc: Description shouldn't end with a full stop.
          desc "NDI® Tools a free suite of applications and tools for NDI VoIP streaming."
                                                                                    ^

--- Casks/ndi-tools.rb
+++ Casks/ndi-tools.rb
@@ -4,11 +4,11 @@ cask "ndi-tools" do
 
   url "https://downloads.ndi.tv/Tools/NDIToolsInstaller.pkg"
   name "ndi-tools"
-  desc "NDI® Tools a free suite of applications and tools for NDI VoIP streaming."
+  desc "NDI® Tools a free suite of applications and tools for NDI VoIP streaming"
   homepage "https://www.ndi.tv/tools/"
 
   pkg "NDIToolsInstaller.pkg"
 
-  uninstall quit:    "com\.newtek\.(Application-Mac-NDI-AccessManager|NDI-Toolscom\.newtek\.Application-Mac-NDI-ScanConverter|Test-Patterns-Mac-|Application-Mac-NDI-StudioMonitor|Application-Mac-NDI-VirtualInput)",
-            pkgutil: "com\.newtek\.(NDI(-Tools|-HX-Driver|\.prefpane|-Transmit-AdobeCC|-Transmit-AdobeCC)|HAL\.NDIaudioplugin|DAL(\.NDIpluginlaunchdaemon|\.NDIplugin)|NewTek-Import-SpeedHQ|Test-Patterns-Mac-|ndi\.recording)"
+  uninstall quit:    "com.newtek.(Application-Mac-NDI-AccessManager|NDI-Toolscom.newtek.Application-Mac-NDI-ScanConverter|Test-Patterns-Mac-|Application-Mac-NDI-StudioMonitor|Application-Mac-NDI-VirtualInput)",
+            pkgutil: "com.newtek.(NDI(-Tools|-HX-Driver|.prefpane|-Transmit-AdobeCC|-Transmit-AdobeCC)|HAL.NDIaudioplugin|DAL(.NDIpluginlaunchdaemon|.NDIplugin)|NewTek-Import-SpeedHQ|Test-Patterns-Mac-|ndi.recording)"
 end

77c969743f5adab9d661df8e6d8a24b88e992a3a
James Cuzella <james.cuzella@lyraphase.com>
2023-07-19 02:50:42 -0600

    ndi-tools: Fix Cask/Desc: Description shouldn't start with the cask name.
    Warning was:
    
           /home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/lyraphase/homebrew-av-casks/Casks/ndi-tools.rb:7:9: C: [Correctable] Cask/Desc: Description shouldn't start with the cask name.
            desc "NDI Tools a free suite of applications and tools for NDI VoIP streaming"
                  ^^^^^^^^^

--- Casks/ndi-tools.rb
+++ Casks/ndi-tools.rb
@@ -4,7 +4,7 @@ cask "ndi-tools" do
 
   url "https://downloads.ndi.tv/Tools/NDIToolsInstaller.pkg"
   name "ndi-tools"
-  desc "NDI Tools a free suite of applications and tools for NDI VoIP streaming"
+  desc "Free suite of applications and tools for NDI VoIP streaming"
   homepage "https://www.ndi.tv/tools/"
 
   pkg "NDIToolsInstaller.pkg"
